8847dcd5f9371b230c118f4c7b8eaf86acb19368
Truong Nguyen <truong.nguyenxuan@asnet.com.vn>
2018-06-20 16:38:08 +0700

    add NetModule

---
+++ WeatherAac/app/src/main/java/com/aac/aac/weatheraac/di/NetModule.java
@@ -0,0 +1,76 @@
+package com.aac.aac.weatheraac.di;
+
+import com.aac.aac.weatheraac.App;
+import com.aac.aac.weatheraac.BuildConfig;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+public class NetModule {
+
+    private String baseUrl;
+
+    public NetModule(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    Cache provideHttpCache(App app) {
+        final int cacheSize = 10 * 1024 * 1024;     // 10 MB
+        return new Cache(app.getCacheDir(), cacheSize);
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        return new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES).create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient.Builder provideOkhttpBuilder() {
+        return new OkHttpClient.Builder();
+    }
+
+    @Singleton
+    @Provides
+    OkHttpClient provideOkHttpClient(Cache cache, OkHttpClient.Builder builder) {
+        builder.cache(cache)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS);
+
+        if (BuildConfig.BUILD_TYPE.equalsIgnoreCase("debug") || BuildConfig.FLAVOR.equalsIgnoreCase("dev")) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(loggingInterceptor);
+        }
+        return builder.build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+}
